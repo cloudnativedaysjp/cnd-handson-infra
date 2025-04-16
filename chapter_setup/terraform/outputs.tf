2ea9556a92abbf59020ccaebfc1828df9a8cece1
takei-ryuichi <takechiyo.0127@gmail.com>
2025-04-16 23:46:41 +0900

    #444 chapter_setupをIaC化

---
+++ chapter_setup/terraform/outputs.tf
@@ -0,0 +1,33 @@
+output "instance_ips" {
+  value = aws_instance.ubuntu_instance[*].public_ip
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.handson_bucket.bucket
+}
+
+output "download_url_api_endpoint" {
+  value = "https://${aws_api_gateway_rest_api.download_url_api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.download_url_stage.stage_name}/generate_url"
+}
+
+output "ec2_instance_names" {
+  value = aws_instance.ubuntu_instance[*].tags["Name"]
+}
+
+output "private_key_s3_urls" {
+  value = [
+    for i in range(var.num_students) :
+    "s3://${aws_s3_bucket.handson_bucket.bucket}/keys/student${i + 1}.pem"
+  ]
+}
+
+output "hosts_file_s3_urls" {
+  value = [
+    for i in range(var.num_students) :
+    "s3://${aws_s3_bucket.handson_bucket.bucket}/student${i + 1}/hosts.txt"
+  ]
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.generate_download_url_lambda.function_name
+}

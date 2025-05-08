07eda159b32172e2448aa6d325d2d5330922fc8f
Ryuichi Takei <97933671+takei-ryuichi@users.noreply.github.com>
2025-05-09 05:10:55 +0900

    Update main.tf
    Corrected points raised in review.

--- chapter_setup/terraform/main.tf
+++ chapter_setup/terraform/main.tf
@@ -128,7 +128,7 @@ resource "aws_instance" "ubuntu_instance" {
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.handson_keys[count.index].key_name
   subnet_id                   = aws_subnet.handson_subnet.id
-  security_groups             = [aws_security_group.handson_sg.id]
+  vpc_security_group_ids      = [aws_security_group.handson_sg.id]
   associate_public_ip_address = true
 
   user_data = <<-EOF

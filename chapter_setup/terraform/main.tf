6f406c2da2d86501520585584b0849587ecc4981
Ryuichi Takei <97933671+takei-ryuichi@users.noreply.github.com>
2025-05-17 12:45:38 +0900

    Update main.tf

--- chapter_setup/terraform/main.tf
+++ chapter_setup/terraform/main.tf
@@ -155,7 +155,7 @@ resource "aws_instance" "ubuntu_instance" {
     apt-get update
     apt-get install -y curl vim git unzip gnupg lsb-release ca-certificates dstat jq
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=\\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \\$(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+    echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' > /etc/apt/sources.list.d/docker.list
     apt-get update
     apt-get install -y docker-ce docker-ce-cli containerd.io
   EOF

4b0d2d21879fab6c3189c5e25f7d32fd68cf798b
hayama17 <kohei.hayama.9r@gmail.com>
2026-05-13 10:48:38 +0900

    name変更

--- chapter_setup/terraform/dns.tf
+++ chapter_setup/terraform/dns.tf
@@ -6,7 +6,7 @@ resource "aws_route53_record" "www" {
   count = length(aws_instance.ubuntu_instance)
 
   zone_id = aws_route53_zone.handson.zone_id
-  name    = "www.vm0" + count.index + ".handson.cloudnativedays.jp"
+  name    = "www.vm0${count.index}.handson.cloudnativedays.jp"
   type    = "A"
   ttl     = 300
   records = [aws_instance.ubuntu_instance[count.index].public_ip]

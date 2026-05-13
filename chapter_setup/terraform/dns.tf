1ec786395f3eef8a48857822ac1497fae45e51da
hayama17 <kohei.hayama.9r@gmail.com>
2026-05-13 11:32:02 +0900

    ワイルドカード対応

--- chapter_setup/terraform/dns.tf
+++ chapter_setup/terraform/dns.tf
@@ -1,49 +1,52 @@
 resource "aws_route53_zone" "handson" {
   name = "handson.cloudnativedays.jp"
 }
-locals {
-  host_subdomains = [
-    "hello-world",
-    "rollout",
-    "blue",
-    "green",
-    "app",
-    "cnd-web",
-    "prometheus",
-    "grafana",
-    "jaeger",
-    "argocd",
-    "app.argocd",
-    "dev.kustomize.argocd",
-    "prd.kustomize.argocd",
-    "helm.argocd",
-    "app-preview.argocd",
-    "kiali",
-    "kiali-ambient",
-    "app.cilium",
-    "hubble.cilium",
-    "pyroscope",
-    "app.cicd",
-  ]
-
-  dns_records = {
-    for pair in flatten([
-      for i in range(var.num_students) : [
-        for host in local.host_subdomains : {
-          key = "${host}.vm${format("%02d", i + 1)}"
-          idx = i
-        }
-      ]
-    ]) : pair.key => pair
-  }
-}
-
-resource "aws_route53_record" "host_records" {
-  for_each = local.dns_records
+resource "aws_route53_record" "wildcard_records" {
+  count = var.num_students
 
   zone_id = aws_route53_zone.handson.zone_id
-  name    = "${each.key}.handson.cloudnativedays.jp"
+  name    = "*.vm${format("%02d", count.index + 1)}.handson.cloudnativedays.jp"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.ubuntu_instance[each.value.idx].public_ip]
+  records = [aws_instance.ubuntu_instance[count.index].public_ip]
+}
+
+resource "aws_route53_record" "argocd_wildcard_records" {
+  count = var.num_students
+
+  zone_id = aws_route53_zone.handson.zone_id
+  name    = "*.argocd.vm${format("%02d", count.index + 1)}.handson.cloudnativedays.jp"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.ubuntu_instance[count.index].public_ip]
+}
+
+resource "aws_route53_record" "kustomize_argocd_wildcard_records" {
+  count = var.num_students
+
+  zone_id = aws_route53_zone.handson.zone_id
+  name    = "*.kustomize.argocd.vm${format("%02d", count.index + 1)}.handson.cloudnativedays.jp"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.ubuntu_instance[count.index].public_ip]
+}
+
+resource "aws_route53_record" "cilium_wildcard_records" {
+  count = var.num_students
+
+  zone_id = aws_route53_zone.handson.zone_id
+  name    = "*.cilium.vm${format("%02d", count.index + 1)}.handson.cloudnativedays.jp"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.ubuntu_instance[count.index].public_ip]
+}
+
+resource "aws_route53_record" "cicd_wildcard_records" {
+  count = var.num_students
+
+  zone_id = aws_route53_zone.handson.zone_id
+  name    = "*.cicd.vm${format("%02d", count.index + 1)}.handson.cloudnativedays.jp"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.ubuntu_instance[count.index].public_ip]
 }

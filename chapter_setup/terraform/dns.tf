e23fa0bd82500f932ae51044b884b5b79ded014d
hayama17 <kohei.hayama.9r@gmail.com>
2026-05-13 10:45:50 +0900

    countの修正

--- chapter_setup/terraform/dns.tf
+++ chapter_setup/terraform/dns.tf
@@ -3,11 +3,11 @@ resource "aws_route53_zone" "handson" {
 }
 
 resource "aws_route53_record" "www" {
-  count = var.num_students
+  count = length(aws_instance.ubuntu_instance)
 
   zone_id = aws_route53_zone.handson.zone_id
-  name    = "www.handson.cloudnativedays.jp"
+  name    = "www.vm0" + count.index + ".handson.cloudnativedays.jp"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.ubuntu_instance[var.num_students].public_ip]
+  records = [aws_instance.ubuntu_instance[count.index].public_ip]
 }

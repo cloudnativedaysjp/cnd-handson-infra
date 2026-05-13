711a607fb9a01647d300a816f897175f06cd01a6
hayama17 <kohei.hayama.9r@gmail.com>
2026-05-13 11:25:16 +0900

    hosts.template対応

--- chapter_setup/terraform/dns.tf
+++ chapter_setup/terraform/dns.tf
@@ -1,13 +1,49 @@
 resource "aws_route53_zone" "handson" {
   name = "handson.cloudnativedays.jp"
 }
+locals {
+  host_subdomains = [
+    "hello-world",
+    "rollout",
+    "blue",
+    "green",
+    "app",
+    "cnd-web",
+    "prometheus",
+    "grafana",
+    "jaeger",
+    "argocd",
+    "app.argocd",
+    "dev.kustomize.argocd",
+    "prd.kustomize.argocd",
+    "helm.argocd",
+    "app-preview.argocd",
+    "kiali",
+    "kiali-ambient",
+    "app.cilium",
+    "hubble.cilium",
+    "pyroscope",
+    "app.cicd",
+  ]
 
-resource "aws_route53_record" "www" {
-  count = length(aws_instance.ubuntu_instance)
+  dns_records = {
+    for pair in flatten([
+      for i in range(var.num_students) : [
+        for host in local.host_subdomains : {
+          key = "${host}.vm${format("%02d", i + 1)}"
+          idx = i
+        }
+      ]
+    ]) : pair.key => pair
+  }
+}
+
+resource "aws_route53_record" "host_records" {
+  for_each = local.dns_records
 
   zone_id = aws_route53_zone.handson.zone_id
-  name    = "www.vm0${count.index + 1}.handson.cloudnativedays.jp"
+  name    = "${each.key}.handson.cloudnativedays.jp"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.ubuntu_instance[count.index].public_ip]
+  records = [aws_instance.ubuntu_instance[each.value.idx].public_ip]
 }
